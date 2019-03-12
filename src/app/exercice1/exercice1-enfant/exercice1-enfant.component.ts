10897139b3db67c6bcc9c20e3fa167c4c0119a4b
ahmed <chibi.ahmed@gmail.com>
2019-03-12 16:10:10 +0100

    projet 3 chapitre 4 output input exercice1

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, Input} from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1-enfant.component.css']
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Output() evPlusUn : EventEmitter<number> = new EventEmitter<number>();
+  @Output() evMoinsUn : EventEmitter<number> = new EventEmitter<number>();
+
+  //pour recuperer le resultat dans le composant enfant
+  @Input() result : number;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  incrementer() {
+    this.evPlusUn.emit(1);
+  }
+  decrementer(){
+    this.evMoinsUn.emit(1);
+  }
 }
--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -6,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1.component.css']
 })
 export class Exercice1Component implements OnInit {
-  public compteur: number = 0; 
+  /*public compteur: number = 0;*/
+  public  result : number = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
+  incrementerDeUn(un:number){
+    console.log(un);
+    this.result=this.result+un;
 
+  }
+  decrementerDeUn(un:number){
+    console.log(un);
+    this.result=this.result-un;
+
+  }
 }
